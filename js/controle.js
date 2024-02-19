4ce390584c1cdb95d5a7a579443f04ab1ffbe6fd
Bruno Stefano de Souza <40743118+vonkoln@users.noreply.github.com>
2024-02-19 04:13:28 +0000

    ...

--- js/controle.js
+++ js/controle.js
@@ -1,44 +1,68 @@
-document.addEventListener("DOMContentLoaded", () => {
-let contador = 0
-let input = document.getElementById('InputTarefa');
-let btnAdd = document.getElementById('btn-add');
-let main = document.getElementById('areaLista');
+let contador = 0;
+let input = document.getElementById("inputTarefa");
+let btnAdd = document.getElementById("btn-add");
+let main = document.getElementById("areaLista");
 
 function addTarefa() {
-    //Pegar o valor digitado no input
-    let valorInput = input.value;
+  //PEGAR O VALOR DIGITADO NO INPUT
+  let valorInput = input.value;
 
-    //Se não for vazio, nem nulo, nem indefinido
-    if((valorInput !== "") && (valorInput !== null) && (valorInput !== undefined)) {
-       ++contador
-       
-        let novoItem = `<div id="${contador}" class="item">
-        <div class="item-icone">
-        <ion-icon name="ellipse-outline"></ion-icon>
-         </div>
-           <div class="item-nome">
-          ${valorInput}
-          </div>
-          <div class="item-botao">
-          <button onclick="deletar()" class="delete" ><ion-icon name="trash-outline"></ion-icon> Deletar</button>
-           </div>
-            </div>`;
+  //SE NÃO FOR VAZIO, NEM NULO, NEM INDEFINIDO
+  if (valorInput !== "" && valorInput !== null && valorInput !== undefined) {
+    ++contador;
 
-        main.innerHTML += novoItem;
+    let novoItem = `<div id="${contador}" class="item">
+        <div onclick="marcarTarefa(${contador})" class="item-icone">
+            <i id="icone_${contador}" class="mdi mdi-circle-outline"></i>
+        </div>
+        <div onclick="marcarTarefa(${contador})" class="item-nome">
+            ${valorInput}
+        </div>
+        <div class="item-botao">
+            <button onclick="deletar(${contador})" class="delete"><i class="mdi mdi-delete"></i> Deletar</button>
+        </div>
+    </div>`;
 
+    //ADICIONAR NOVO ITEM NO MAIN
+    main.innerHTML += novoItem;
 
-
-        input.value = "";
-        input.focus();
-
-
-    }
-      
+    //ZERAR OS CAMPINHOS
+    input.value = "";
+    input.focus();
+  }
 }
-input.addEventListener("keyup", function(event){
-    if(event.keyCode === 13){
-        event.preventDefault();
-        btnAdd.click();
-    }
-})
-}) 
+
+function deletar(id) {
+  var tarefa = document.getElementById(id);
+  tarefa.remove();
+}
+
+function marcarTarefa(id) {
+  var item = document.getElementById(id);
+  var classe = item.getAttribute("class");
+  console.log(classe);
+
+  if (classe == "item") {
+    item.classList.add("clicado");
+
+    var icone = document.getElementById("icone_" + id);
+    icone.classList.remove("mdi-circle-outline");
+    icone.classList.add("mdi-check-circle");
+
+    item.parentNode.appendChild(item);
+  } else {
+    item.classList.remove("clicado");
+
+    var icone = document.getElementById("icone_" + id);
+    icone.classList.remove("mdi-check-circle");
+    icone.classList.add("mdi-circle-outline");
+  }
+}
+
+input.addEventListener("keyup", function (event) {
+  //SE TECLOU ENTER (13)
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    btnAdd.click();
+  }
+});
